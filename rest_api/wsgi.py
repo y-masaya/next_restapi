338774a5cc9b99a3de57354691a03c37743b0070
y-masaya <my.927ac@gmail.com>
2023-09-21 01:50:18 +0900

    initial commit2

--- rest_api/wsgi.py
+++ rest_api/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for rest_api_project2 project.
+WSGI config for rest_api project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -8,9 +8,9 @@ https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
 """
 
 import os
-
+from dj_static import Cling
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'rest_api_project2.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'rest_api.settings')
 
-application = get_wsgi_application()
+application = Cling(get_wsgi_application())
